5b94cbb843c3cfa64217d5ebecc0673fc84aa1dc
jaykhans <jahongirnagmatov@gmail.com>
2022-02-04 10:04:23 +0500

    update deleteHandler and useState

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,17 +1,24 @@
+import { useState } from "react";
+import Modal from './Modal';
+import Backdrop from './Backdrop';
+
 function Todo(props) {
-    function deleteHandler() {
-        
-    }
-    return (
-    
-    <div className='card'>
+  const [modalIsOpen, setModalIsOpen] = useState(false); //useState(false) false means it will not allow to execute this function when page renders. To execute it we should set it true.
+  function deleteHandler() {
+    setModalIsOpen(true);
+  }
+  return (
+    <div className="card">
       <h2>{props.text}</h2>
-      <div className='actions'>
-        <button className="btn" onClick={deleteHandler}>Delete</button>
+      <div className="actions">
+        <button className="btn" onClick={deleteHandler}>
+          Delete
+        </button>
       </div>
+      {modalIsOpen && <Modal />}
+      {modalIsOpen && <Backdrop />}
     </div>
-  
-    ); 
+  );
 }
 
 export default Todo;
