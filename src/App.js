fb249675e9cdb98a8ff76d3040fd94b62eafbb40
jaykhans <jahongirnagmatov@gmail.com>
2022-02-04 10:51:50 +0500

    remove the components and reset the project to new project

--- src/App.js
+++ src/App.js
@@ -4,11 +4,7 @@ import Todo from './components/Todo';
 function App() {
   return (
     <div>
-      <h1>My ToDos</h1>
-      <Todo text='Learn React' />
-      <Todo text= 'Master React'/>
-      <Todo text='Be React Guru'/>
-      
+      Hello again
     </div>
   );
 }
