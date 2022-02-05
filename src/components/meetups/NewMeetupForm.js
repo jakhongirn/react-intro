8a26d40ece8ddd31689296164c89f02c5653df30
jaykhans <jahongirnagmatov@gmail.com>
2022-02-05 17:51:00 +0500

    stores all new form data to meetup const

--- src/components/meetups/NewMeetupForm.js
+++ src/components/meetups/NewMeetupForm.js
@@ -1,35 +1,59 @@
 import Card from "../ui/Card";
-import classes from './styles/NewMeetupForm.module.css';
-
-
+import classes from "./styles/NewMeetupForm.module.css"; //imports al classes from css file. Note that you should write css file as {filename}.module.css
+import { useRef } from "react";
 
 function NewMeetupForm() {
-    return (  
-        <Card>
-            <form className={classes.form} >
-                <div className={classes.control}>
-                    <label htmlFor='title'>Meetup Title</label>
-                    <input type='text' required id='title' />
-                </div>
-                <div className={classes.control}>
-                    <label htmlFor='title'>Metup Image</label>
-                    <input type='url' required id='image' />
-                </div>
-                <div className={classes.control}>
-                    <label htmlFor='address'>Address</label>
-                    <input type='url' required id='address' />
-                </div>
-                <div className={classes.control}>
-                    <label htmlFor='description'>Description</label>
-                    <textarea id='description' required rows='5'> </textarea>
-                </div>
-                <div className={classes.actions}>
-                    <button >Add Meetup</button>
-                    </div> 
+  const titleInputRef = useRef(); //useRef gives the value of each input from form
+  const imageInputRef = useRef();
+  const addressInputRef = useRef();
+  const descriptionInputRef = useRef();
 
-            </form>
-        </Card>
-    );
+
+  // function that handles when form submits by user
+  function submitHandler(event) {
+    event.preventDefault(); //prevent refresh the page
+    const enteredTitle = titleInputRef.current.value;
+    const enteredImage = imageInputRef.current.value;
+    const enteredAddress = addressInputRef.current.value;
+    const enteredDescription = descriptionInputRef.current.value;
+
+    const meetupData = {
+        title: enteredTitle,
+        image: enteredImage,
+        address: enteredAddress,
+        description: enteredDescription,
+    };
+    
+    console.log(meetupData);
+  }
+
+  return (
+    <Card>
+      <form className={classes.form} onSubmit={submitHandler}>
+        <div className={classes.control}>
+          <label htmlFor="title">Meetup Title</label>
+          <input type="text" required id="title" ref={titleInputRef} />
+        </div>
+        <div className={classes.control}>
+          <label htmlFor="image">Metup Image</label>
+          <input type="url" required id="image" ref={imageInputRef} />
+        </div>
+        <div className={classes.control}>
+          <label htmlFor="address">Address</label>
+          <input type="text" required id="address" ref={addressInputRef} />
+        </div>
+        <div className={classes.control}>
+          <label htmlFor="description">Description</label>
+          <textarea id="description" required rows="5" ref={descriptionInputRef}>
+
+          </textarea>
+        </div>
+        <div className={classes.actions}>
+          <button>Add Meetup</button>
+        </div>
+      </form>
+    </Card>
+  );
 }
 
 export default NewMeetupForm;
