608c48202a60a742ba571392fead2aab9a7c1404
jaykhans <jahongirnagmatov@gmail.com>
2022-02-05 18:10:46 +0500

    connect firebase and store the data with post method

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,14 +1,23 @@
-import NewMeetupForm from '../components/meetups/NewMeetupForm';
-
+import NewMeetupForm from "../components/meetups/NewMeetupForm";
 
 function NewMeetupPage() {
-    function AddMeetupHandler(meetupData){
-
-    }
+  function AddMeetupHandler(meetupData) {
+    fetch(
+      "https://react-intro-c9930-default-rtdb.firebaseio.com/meetups.json",
+      {
+        method: "POST",
+        body: JSON.stringify(meetupData), //Convert data into JSON
+        headers: {
+          "Content-Type": "application/json",
+        },
+      } //It's api rules of Firebase
+    ); //Builtin vanilla Javascript function
+  }
+  
   return (
     <section>
       <h1>Add New Meetup</h1>
-        <NewMeetupForm onAddMeetup={AddMeetupHandler}/>
+      <NewMeetupForm onAddMeetup={AddMeetupHandler} />
     </section>
   );
 }
