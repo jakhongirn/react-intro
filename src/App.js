57a7b83c2baa32332a5025e9f181f5fdc7fcbeeb
jaykhans <jahongirnagmatov@gmail.com>
2022-02-05 12:44:27 +0500

    navigation for all pages

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import { Routes, Route } from "react-router-dom";
 import AllMeetupsPage from "./pages/AllMeetups";
 import FavoritePage from "./pages/Favorites";
 import NewMeetupPage from "./pages/NewMeetup";
+import MainNavigation from "./components/layout/MainNavigation";
 
 function App() {
   return (
     <div>
+      <MainNavigation />
       <Routes>
-        <Route path="/" element={<AllMeetupsPage />} />
+        <Route path="/" element={<AllMeetupsPage />} exact/>
         <Route path="/new-meetup" element={<NewMeetupPage />} />
         <Route path="/favorites" element={<FavoritePage />} />
       </Routes>
