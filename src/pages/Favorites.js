943c425255ee287fa042666bfb8fd4b5799db4f0
jaykhans <jahongirnagmatov@gmail.com>
2022-02-07 17:24:01 +0500

    add badge to favorites

--- src/pages/Favorites.js
+++ src/pages/Favorites.js
@@ -4,11 +4,21 @@ import FavoritesContext from '../store/favorites-context';
 import MeetupList from '../components/meetups/MeetupList';
 
 function FavoritePage() {
-    const favortitesCtx = useContext(FavoritesContext);
+    const favoritesCtx = useContext(FavoritesContext);
+
+    let content;
+
+    if (favoritesCtx.totalFavorites === 0) {
+        content = <p>You got no favorites yet. Start adding some?</p>
+    }
+    else {
+        content = <MeetupList meetups={favoritesCtx.favorites} />
+    }
+
     return (
         <section>
             <h1>My Favorites</h1>
-            <MeetupList meetups={favortitesCtx.favorites} />
+            {content}
         </section>
     );
 }
