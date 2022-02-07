278a0835a114f90ca89ae05705edf7e1a80ec5ed
jaykhans <jahongirnagmatov@gmail.com>
2022-02-07 17:06:23 +0500

    show favorites to favorite page

--- src/pages/Favorites.js
+++ src/pages/Favorites.js
@@ -1,5 +1,16 @@
+import {useContext} from 'react';
+
+import FavoritesContext from '../store/favorites-context';
+import MeetupList from '../components/meetups/MeetupList';
+
 function FavoritePage() {
-    return <div>FavoritePage</div>;
+    const favortitesCtx = useContext(FavoritesContext);
+    return (
+        <section>
+            <h1>My Favorites</h1>
+            <MeetupList meetups={favortitesCtx.favorites} />
+        </section>
+    );
 }
 
 export default FavoritePage;
