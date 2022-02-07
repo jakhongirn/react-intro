f41b35df07ebfa8f722ef551479db37f8d298022
jaykhans <jahongirnagmatov@gmail.com>
2022-02-07 13:31:35 +0500

    favorite context

--- src/store/favorites-context.js
+++ src/store/favorites-context.js
@@ -1,43 +1,43 @@
 import { createContext, useState } from "react";
 
-const FavoritesContext = createContext( {
-    favorites: [],
-    totalFavorites: 0,
-    addFavorite: (favoriteMeetup) => {},
-    removeFavorite: (meetupId) => {},
-    itemIsFavorite: (meetupId) => {}
+const FavoritesContext = createContext({
+  favorites: [],
+  totalFavorites: 0,
+  addFavorite: (favoriteMeetup) => {},
+  removeFavorite: (meetupId) => {},
+  itemIsFavorite: (meetupId) => {},
 });
 
 export function FavoritesContextProvider(props) {
-    const [userFavorites, setUserFavorites] = useState([]);
+  const [userFavorites, setUserFavorites] = useState([]);
 
-    function addFavoriteHandler(favoriteMeetup) {
-        setUserFavorites((prevUserFavorites) => {
-            return prevUserFavorites.concat(favoriteMeetup);
-        }); //adding new favorite meetup .concat is push to the list
-    }
-    function removeFavoriteHandler(meetupId){
-        setUserFavorites(prevUserFavorites => {
-            return prevUserFavorites.filter(meetup => {
-                meetup.id !== meetupId
-            });
-        });
-    }
-    function itemIsFavoriteHandler(meetupId){
-        return userFavorites.some(meetup => meetup.id == meetupId);
-    };
+  function addFavoriteHandler(favoriteMeetup) {
+    setUserFavorites((prevUserFavorites) => {
+      return prevUserFavorites.concat(favoriteMeetup);
+    }); //adding new favorite meetup .concat is push to the list
+  }
+  function removeFavoriteHandler(meetupId) {
+    setUserFavorites((prevUserFavorites) => {
+      return prevUserFavorites.filter((meetup) => meetup.id !== meetupId);
+    });
+  }
+  function itemIsFavoriteHandler(meetupId) {
+    return userFavorites.some((meetup) => meetup.id === meetupId);
+  }
 
-    const context ={
-        favorites: userFavorites,
-        totalFavorites: userFavorites.length,
-        addFavorite: addFavoriteHandler,
-        removeFavorit: removeFavoriteHandler,
-        itemIsFavorite: itemIsFavoriteHandler
-    };
+  const context = {
+    favorites: userFavorites,
+    totalFavorites: userFavorites.length,
+    addFavorite: addFavoriteHandler,
+    removeFavorit: removeFavoriteHandler,
+    itemIsFavorite: itemIsFavoriteHandler,
+  };
 
-    return <FavoritesContext.Provider value={context}>  
-        {props.children}   //all the contents inside FavoritesContextProvider will be wrapped
-    </FavoritesContext.Provider>
-}
+  return (
+    <FavoritesContext.Provider value={context}>
+      {props.children}
+    </FavoritesContext.Provider> 
+  ); 
+}//all the contents inside FavoritesContextProvider will be wrapped
 
-export default FavoritesContextProvider
+export default FavoritesContextProvider;
